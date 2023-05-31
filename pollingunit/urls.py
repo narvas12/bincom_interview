7450eacb180846a28a4c66f9d9fe16efa26cd457
narvas12 <ebukaemmanuel71@gmail.com>
2023-05-31 01:59:50 +0100

    redo

--- pollingunit/urls.py
+++ pollingunit/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('announced-pu-results/', views.announced_pu_results, name='announced_pu_results'),
     path('summed-total-result/', views.summed_total_result, name='summed_total_result'),
+    path('add-polling-unit-results/', views.add_polling_unit_results, name='add_polling_unit_results'),
 ]
--- pollingunit/views.py
+++ pollingunit/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-from .models import AnnouncedPuResults
-from .models import PollingunitAnnouncedlgaresult
-
+from django.shortcuts import render, redirect
+from .models import *
 
 def announced_pu_results(request):
     results = AnnouncedPuResults.objects.all()
@@ -9,19 +7,61 @@ def announced_pu_results(request):
 
 
 
-def summed_total_result(request):
-    lgas = PollingunitAnnouncedlgaresult.objects.values_list('lga_name', flat=True).distinct()
-    selected_lga = request.GET.get('lga', None)
 
-    if selected_lga:
-        total_result = PollingunitAnnouncedlgaresult.objects.filter(lga_name=selected_lga).aggregate(Sum('party_score'))
-    else:
-        total_result = None
+def summed_total_result(request):
+    # Retrieve all the local governments
+    local_governments = AnnouncedLgaResults.objects.all()
+
+    if request.method == 'POST':
+        selected_lga = request.POST.get('local_government')
+
+        # Retrieve the polling unit results for the selected local government
+        results = AnnouncedLgaResults.objects.filter(lga_name=selected_lga)
+
+        # Calculate the summed total result
+        total_result = sum(result.party_score for result in results)
+
+        context = {
+            'local_governments': local_governments,
+            'selected_lga': selected_lga,
+            'total_result': total_result,
+        }
+
+        return render(request, 'lga_total_result.html', context)
 
     context = {
-        'lgas': lgas,
-        'selected_lga': selected_lga,
-        'total_result': total_result,
+        'local_governments': local_governments,
     }
 
-    return render(request, 'myapp/summed_total_result.html', context)
+    return render(request, 'lga_total_result.html', context)
+
+
+
+
+
+def add_polling_unit_results(request):
+    parties = Party.objects.all()
+    polling_units = PollingUnit.objects.all()
+
+    if request.method == 'POST':
+        polling_unit_id = request.POST.get('polling_unit')
+        for party in parties:
+            party_abbreviation = party.partyid
+            party_score = request.POST.get(f'party_{party_abbreviation}')
+
+            PollingunitAnnouncedpuresult.objects.create(
+                polling_unit_uniqueid=polling_unit_id,
+                party_abbreviation=party_abbreviation,
+                party_score=party_score,
+                entered_by_user=request.user.username,  # Change if you have user authentication
+                # Add other fields as necessary
+            )
+
+        return redirect('polling_unit_results')  # Redirect to a page after saving the results
+
+    context = {
+        'parties': parties,
+        'polling_units': polling_units,
+    }
+
+    return render(request, 'new_pu_results.html', context)
